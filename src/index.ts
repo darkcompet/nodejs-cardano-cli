08fd066611884dea5ff2e308e8880d6d0c28a471
DarkCompet <darkcompet@gmail.com>
2022-07-17 08:05:03 +0700

    Change

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 /// This is main entry point of the module.
 /// Only files be exported here can be used at other modules.
 
-export * from "./cardano-cli";
-export * from "./model";
+export * from './cardano-cli';
+export * from './model';
 export * from './constant'
